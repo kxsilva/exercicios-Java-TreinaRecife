19fb162ce7f37b58f8823b48cccba928e189fd5c
Kaynã Xavier <kayna.xavier@ufpe.br>
2023-01-13 19:44:09 -0300

    correcao

--- de_21_a_30/Ex22.java
+++ de_21_a_30/Ex22.java
@@ -10,8 +10,6 @@ public class Ex22 {
 		/*
 		 * A empresa local de abastecimento de água, a Saneamento Básico da Cidade
 (SBC), está promovendo uma campanha de conservação de água, distribuindo
-13
-PROF. ALEXANDRE DE SOUZA JR
 cartilhas e promovendo ações demonstrando a importância da água para a vida e
 para o meio ambiente. Para incentivar mais ainda a economia de água, a SBC
 alterou os preços de seu fornecimento de forma que, proporcionalmente, aqueles
@@ -25,6 +23,30 @@ que o consumidor não tenha consumido água). Acima de 10 m3
 subsequente tem um valor diferente, dependendo da faixa de consumo. A SBC
 cobra apenas por quantidades inteiras de metros cúbicos consumidos. A tabela
 abaixo especifica o preço por metro cúbico para cada faixa de consumo:
+
+Faixa de Consumo (m3
+) Preço (por m3
+)
+até 10		
+11 a 30
+31 a 100
+100 em diante
+incluído na franquia
+R$ 1
+R$ 2
+R$ 5
+Assim, por exemplo, se o consumo foi de 120 m3
+, o valor da conta é:
+● 7 reais da assinatura básica;
+● 20 reais pelo consumo no intervalo 11 — 30 m3
+;
+● 140 reais pelo consumo no intervalo 31 — 100 m3
+;
+● 400 reais pelo consumo no intervalo 101 — 120 m3
+.
+Logo, o valor total da conta de água é R$ 267. Tarefa Escreva um programa que,
+dado o consumo de em m3
+, calcula o valor da conta de água daquela residência
 		 * */
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
